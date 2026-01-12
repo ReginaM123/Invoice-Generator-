e75b1271e806aa1ed3061431a492ba81d9b7ff6a
Karabo Motsomi <134011817+ReginaM123@users.noreply.github.com>
2026-01-12 05:38:16 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import InvoiceForm from "./components/InvoiceForm";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Invoice Generator</h1>
+      <InvoiceForm />
+    </div>
+  );
+}
+
+export default App;
